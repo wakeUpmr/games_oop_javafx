b0c7288c5bb86289d8605c53741b642d7156ab65
Egor Krivonos <karl11karle@gmail.com>
2023-04-16 18:41:53 +0300

    3. Сокобан. Тестовое задание.

--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -3,7 +3,6 @@ package ru.job4j.puzzle;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 public class WinTest {
     @Test
     public void whenVerticalWin() {
